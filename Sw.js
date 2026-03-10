d3a8b34dbfff7817ac03b3fcbd8c3ad375ef303c
wpiekar <87425822+wpiekar@users.noreply.github.com>
2026-03-10 15:11:44 +0100

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,2 +1,34 @@
-test
-test
+const CACHE_NAME = "maroko-roadbook";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/d1.html",
+"/d2.html",
+"/d3.html",
+"/d4.html",
+"/d5.html",
+"/d6.html",
+"/d7.html",
+"/jedzenie.html",
+"/gpx.html",
+"/style.css"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
